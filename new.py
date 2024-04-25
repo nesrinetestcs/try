2a57498ca01b747e6f8410a042cb50f977a60c06
nesrinetestcs <167727080+nesrinetestcs@users.noreply.github.com>
2024-04-25 11:37:15 +0100

    Update new.py

--- new.py
+++ new.py
@@ -2,7 +2,7 @@ import schedule
 import time
 import os 
 
-# Function to open the file
+# Function to open the file 
 def open_file():
     file_path = "/path/to/your/file.txt"  # Specify the path to your file
     os.system(f"start {file_path}")  # Open the file using the default application
